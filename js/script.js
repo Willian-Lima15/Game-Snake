8a73e5bc2a164022c0b2c0f701a5a54d1b297e5d
Willian Suporte7v <william_slayer@hotmail.com>
2021-05-03 22:50:18 -0300

    Finalizando o jogo

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ let food = {
 
 function criarBG() {
     context.fillStyle = "lightgreen";
-    context.fillReact(0, 0, 16 + box, 16 + box);
+    context.fillReact(0, 0, 16 * box, 16 * box);
 }
 
 function criarCobrinha() {
