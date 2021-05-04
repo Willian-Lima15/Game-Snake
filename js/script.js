d4662f937e85793c9aceda8689de85e591af6d37
Willian Suporte7v <william_slayer@hotmail.com>
2021-05-03 21:39:36 -0300

    Definir movimento

--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@ snake[0] = {
     y: 8 * box
 }
 
+let direction = "right"
+
 function criarBG() {
     context.fillStyle = "lightgreen";
     context.fillReact(0, 0, 16 + box, 16 + box);
@@ -19,5 +21,27 @@ function criarCobrinha() {
     }
 }
 
-criarBG();
-criarCobrinha();
+function iniciarJogo() {
+    criarBG();
+    criarCobrinha();
+
+    let snakex = snake[0].x;
+    let snakey = snake[0].y;
+
+    if (direction == "right") snakex += box;
+    if (direction == "left") snakex -= box;
+    if (direction == "up") snakey += box;
+    if (direction == "down") snakey += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakex,
+        y: snakey
+    }
+
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100);
+
